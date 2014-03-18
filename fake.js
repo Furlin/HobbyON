6286455b2c6c6bf5c4574cec10e5e260dfce0949
Furlin <vitor@furlin.org>
2014-03-18 11:36:33 -0300

    so long

---
+++ fake.js
@@ -0,0 +1,8 @@
+var deck = 60;
+var deck_name = 'Nome do Deck';
+var tour_info = 'Torneio Tal (T2) 11/03/2014';
+var player_name = 'Nome do Jogador';
+var lands = { total: 23, cards: { 0: { name : 'forest', img : 'BNG/forest', num: 11}, 1: { name : 'island', img : 'island', num: 12} } };
+var creatures = { total: 15, cards: { 0: { name : 'bicho1', img : 'bicho1', num: 4}, 1: { name : 'bicho2', img : 'bicho2', num: 4}, 2: { name : 'bicho3', img : 'bicho3', num: 4}, 3: { name : 'bicho4', img : 'bicho4', num: 3} } };
+var other = { total: 22, cards: { 0: { name : 'bicho1', img : 'bicho1', num: 4}, 1: { name : 'bicho2', img : 'bicho2', num: 4}, 2: { name : 'bicho3', img : 'bicho3', num: 4}, 3: { name : 'bicho4', img : 'bicho4', num: 10} } };
+var side = { total: 15, cards: { 0: { name : 'bicho1', img : 'bicho1', num: 4}, 1: { name : 'bicho2', img : 'bicho2', num: 4}, 2: { name : 'bicho3', img : 'bicho3', num: 7} } };
